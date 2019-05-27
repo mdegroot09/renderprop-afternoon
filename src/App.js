68515562962d49a5ec52fe4bf77e480859fe7a57
Mike De Groot <mdegroot09@gmail.com>
2019-05-27 15:06:17 -0600

    updated App.js to render CurrencyConverter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import CurrencyConverter from './Components/CurrencyConverter'
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <h2>Render Props</h2>
+        <CurrencyConverter render={() => {}}/>
+      </>
     );
   }
 }
